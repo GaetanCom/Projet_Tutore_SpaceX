cd3eff3c01f1170c1d52cbb8823f590e2339d955
GaetanCom <comacle.gaetan@gmail.com>
2018-11-17 19:18:53 +0100

    Mise en page de la page d'accueil. Pas encore terminé, revoir la police.

--- index.php
+++ index.php
@@ -4,55 +4,36 @@
   <head>
       <link rel="stylesheet" href="style_footer_header.css"/>
       <link rel="stylesheet" href="style.css"/>
+      <link rel="stylesheet" href="style_accueil.css"/>
       <link rel="icon" href="Images/Logo2.png"/>
       <title> SpaceX </title>
   </head>
 
   <body>
-    <?php include("header.html"); ?>
+      <!-- <?php include("header.html"); ?> -->
+
     <section id="contenu_principal">
-      	<div> aaaaaaaaaaaaaaaaaaaaaaa</div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-      <div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> aaaaaaaaa </div>
-     	<div> BBBBBBBBB </div>
-      </section>
+
+    	<a href="SpaceX.php" class="div_accueil_G" id="spacex_accueil">
+    		<div class="images_accueil"> <img id="image_spacex" src="Images/Logo.png"> </div>
+    	</a>
+
+    	<a href="ElonMusk.php" class="div_accueil_D" id="elonmusk_accueil">
+    		<div class="texts_accueil"> <p> Elon Musk </p> </div>
+    		<div class="images_accueil"> <img src="Images/ElonMusk.png"></div>
+    	</a>
+
+    	<a href="Fusees.php" class="div_accueil_G" id="fusees_accueil">
+    		<div class="images_accueil"> <img src="Images/Falcon9.png"> </div>
+    		<div class="texts_accueil"> <p> Fusées </p> </div>
+		</a>
+
+    	<a href="" class="div_accueil_D" id="concurrents_accueil">
+    		<div class="texts_accueil"> <p> Concurrents </p> </div>
+    		<div class="images_accueil"> <img src="Images/NASA.png"> </div>
+    	</a>
+      	
+    </section>
     
       <?php include("footer.html"); ?>
   </body>
