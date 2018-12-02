4b35aab696ff5f2f28e37a88ca9835462c8d4fd7
Balbalnawfel <nawfelbalbal@gmail.com>
2018-12-02 17:34:37 +0100

    ajout page membre du groupe

---
+++ membres.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+      <link rel="stylesheet" href="style_footer_header.css"/>
+      <link rel="stylesheet" href="style_membre.css"/>
+      <link rel="icon" href="Images/Logo2.png"/>
+      <title> Membre du groupe | SpaceX </title>
+  </head>
+
+  <body>
+    <?php include("header.html"); ?>
+    <div class="cases">
+      <div class="first">
+        <div class="membre">
+          <div class="texte">
+              Comacle Gaëtan <br>
+          </div>
+          Groupe B1
+        </div>
+        <div class="membre">
+          <div class="texte">
+            Balbal Nawfel <br>
+          </div>
+            Groupe B1
+        </div>
+        <div class="membre">
+          <div class="texte">
+            Jean-Baptiste Louis <br>
+          </div>
+            Groupe B1
+        </div>
+      </div>
+      <div class="second">
+        <div class="membre">
+          <div class="texte">
+            Kheloufi Djaber <br>
+          </div>
+            Groupe B1
+        </div>
+        <div class="membre">
+          <div class="texte">
+            Juliat Thomas <br>
+          </div>
+            Groupe B1
+        </div>
+      </div>
+    </div>
+
+
+
+    <?php include("footer.html"); ?>
+  </body>
