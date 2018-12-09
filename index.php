25e3abe0ab20a4afa6efa128e7686da5f7a64e08
Harrakos <45127817+Harrakos@users.noreply.github.com>
2018-12-09 23:17:37 +0100

    page d'accueil
    mise à jour de la page d'accueil

--- index.php
+++ index.php
@@ -9,88 +9,52 @@
   </head>
 
   <body>
-
+    <img id="Logo" src="Images/Logo.png">
     <section id="menu_accueil">
-      <img src="Images/Logo.png">
+      <div id="menu">
+        <ul id="gauche">
+          <a href=""><li>SpaceX</li></a>
+          <a href=""><li>Fusee</li></a>
+        </ul>
+        <ul id="droite">
+          <a href=""><li>Elon Musk</li></a>
+          <a href=""><li>Economie/Droit</li></a>
+        </ul>
+      </div>
+      <div id="actualité">
+        <h1>Actualité</h1>
+      <table>
+        <tr><td><img src="images_accueil/défunt"></td>
+          <td>
+            <h3>SpaceX a envoyé des cendres de défunts dans l'espace</h3>
+            <p>La fusée Falcon 9 a emporté en orbite lundi soir une soixantaine de micro-satellites, dont un petit engin emportant les cendres funéraires d'une centaine de personnes.</p>
+          </td>
+          <td><img src="images_accueil/lancement"></td>
+          <td>
+            <h3>Les images dingues du lancement de Falcon 9 de SpaceX</h3>
+            <p>Le 7 octobre, l'entreprise américaine de vol spatial, SpaceX, a envoyé sa fusée Falcon 9 depuis la base de Vandenberg, en Californie. Le décollage au crépuscule par temps clair a donné lieu à des images absolument sublimes des gaz d'échappement éclairés par le soleil.</p>
+          </td>
+          <tr><td><img src="images_accueil/milliardaire"></td>
+          <td>
+            <h3>Le premier client d'Elon Musk pour aller autour de la Lune est un milliardaire japonais</h3>
+            <p>Elon Musk ne fait décidément rien comme les autres. Le premier passager de sa future fusée d'exploration du système solaire, BFR (pour Big Falcon Rocket), sera... un touriste. Un milliardaire japonais passionné d'art contemporain, Yusaku Maezawa, qui veut aller faire le tour de la Lune avec des artistes qui seront ses invités. </p>
+          </td>
+          <td><img src="images_accueil/starman"></td>
+          <td>
+            <h3>Starman, le conducteur du Roadster de l’espace, a dépassé Mars</h3>
+            <p>Voilà une nouvelle qui va certainement faire rêver bien des astronautes en herbe et des têtes-en-l’air. Starman, ce mannequin engoncé dans une combinaison spatiale au volant du Roadster d’Elon Musk, a dépassé Mars. Son véhicule galactique était au point le plus éloigné du soleil ce 8 novembre, c’est à dire à 249 millions de kilomètres de notre étoile.</p>
+          </td></tr>
+      </table>
 
-      <a href="SpaceX.php" class="div_accueil">
-        <div id="SpaceX" class="partie1">
-        		<p> SpaceX </p>
-          <div class="sous_partie">
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-          </div>
-        </div>
-      </a>
 
-      <div id="ElonMusk" class="partie2">
-        <a href="ElonMusk.php" class="div_accueil">
-          <div class="sous_partie">
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-          </div>
-          <p>Elon Musk</p>
-        </a>
-      </div>
-        
-      <div id="Fusees" class="partie1">
-        <a href="Fusees.php" class="div_accueil">
-          <p>Fusées</p>
-          <div class="sous_partie">
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-          </div>
-        </a>
-        
-      </div>
-    	
-      <div id="concurents" class="partie2">
-        <a href="Concurrents.php" class="div_accueil">
-          <div class="sous_partie">
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-          </div>
-          <p>Concurrents</p>
-        </a>
-      </div>
-    	<div id="économie" class="partie1">
-        <a href="économie.php" class="div_accueil">
-          <p>économie</p>
-          <div class="sous_partie">
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-            <br>
-            aaaaaa
-          </div>
-        </a>
-      </div>
+
+      </div>   
+
+
 
     </section>
 
-    <?php include("footer.html"); ?>
+    <!-- <?php include("footer.html"); ?> -->
 
   </body>
 </html>
